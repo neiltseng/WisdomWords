fedeb7771889ff02bc2f18556f29f5f506262416
neill <neiltseng.isj@gmail.com>
2019-09-10 09:14:15 +0800

    curl animation works

--- app/src/main/java/com/example/wisdomwords/MainActivity.java
+++ app/src/main/java/com/example/wisdomwords/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.wisdomwords;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 import com.google.android.things.pio.Gpio;
@@ -36,6 +40,9 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Gpio mButtonGpio;
     private Boolean mSensorLaunched = Boolean.FALSE;
+    private int pageNumber = 1;
+    private int pageTotal = 3;
+    Boolean dirFlip = Boolean.TRUE;//go forward
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,8 +71,14 @@ public class MainActivity extends Activity {
                 public boolean onGpioEdge(Gpio gpio) {
                     Log.i(TAG, "GPIO changed, signal coming");
                     if( mSensorLaunched == Boolean.FALSE){
-
-                        Log.i(TAG, "Sensor is launched.");
+                        if(pageNumber == pageTotal){
+                            dirFlip = !dirFlip;
+                            pageNumber = 1;
+                        } else{
+                            pageNumber++;
+                        }
+                        flipBook(dirFlip);
+                        Log.i(TAG, "Sensor is launched and flip one page."+pageNumber+","+dirFlip);
                     }
                     // Return true to continue listening to events
                     return true;
@@ -74,5 +87,76 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
         }
+    }
+
+    private void flipBook(boolean direction){
+        CurlView clvw = findViewById(R.id.curlView);
+        // Obtain MotionEvent object
+        long downTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis() + 100;
+        Display mdisp = getWindowManager().getDefaultDisplay();
+        Point mdispSize = new Point();
+        mdisp.getSize(mdispSize);
+        int maxX = mdispSize.x;
+        int maxY = mdispSize.y;
+        float x = 0.0f+maxX;
+        float y = 0.0f+maxY;
+        if(direction){
+            x = 0.0f+maxX;
+            y = 0.0f+maxY;
+        } else {
+            x = 0.0f;
+            y = 0.0f+maxY;
+        }
+// List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        int metaState = 0;
+        MotionEvent motionEventDown = MotionEvent.obtain(
+                downTime,
+                eventTime,
+                MotionEvent.ACTION_DOWN,
+                x,
+                y,
+                metaState
+        );
+        // Dispatch touch event to view
+        clvw.dispatchTouchEvent(motionEventDown);
+        if(direction){
+            x = 0.0f;
+            y = 0.0f+maxY/3;
+        } else {
+            x = 0.0f+maxX;
+            y = 0.0f+maxY/3;
+        }
+// List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        metaState = 0;
+        MotionEvent motionEventMove = MotionEvent.obtain(
+                downTime,
+                eventTime,
+                MotionEvent.ACTION_MOVE,
+                x,
+                y,
+                metaState
+        );
+        // Dispatch touch event to view
+        clvw.dispatchTouchEvent(motionEventMove);
+        if(direction){
+            x = 0.0f;
+            y = 0.0f+maxY/3;
+        } else {
+            x = 0.0f+maxX;
+            y = 0.0f+maxY/3;
+        }
+// List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        metaState = 0;
+        MotionEvent motionEventUp = MotionEvent.obtain(
+                downTime,
+                eventTime,
+                MotionEvent.ACTION_UP,
+                x,
+                y,
+                metaState
+        );
+        // Dispatch touch event to view
+        clvw.dispatchTouchEvent(motionEventUp);
     }
 }
